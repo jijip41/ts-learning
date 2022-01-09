8603e52358563e4143c1b318246c4b6b77e8ec2d
jihye <jiji.pak41@gmail.com>
2022-01-09 18:42:31 +0000

    learn ts type function

--- 1-TYPES/1-2-function.ts
+++ 1-TYPES/1-2-function.ts
@@ -55,5 +55,6 @@
   }
   console.log(addNumbers(1, 2));
   console.log(addNumbers(1, 2, 3, 4));
-  console.log(addNumbers(1, 2, 3, 4, 5, 0));
+  console.log(addNumbers(1, 2, 3, 4, 5));
+  // console.log(addNumbers(1, 2, 'a')) 🙅‍♀️
 }
