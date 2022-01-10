de1482aa0d5e2ac919a822e55c53ca86e43e52d5
jihye <jiji.pak41@gmail.com>
2022-01-10 20:55:56 +0000

    print object after updating its directions

---
+++ 2-types-projects-quiz/game.ts
@@ -0,0 +1,38 @@
+/**
+ * Let's make a game 🕹
+ */
+
+type Position = { x: number; y: number };
+
+const position: Position = {
+  x: 0,
+  y: 0,
+};
+function move(direction: string): Position {
+  switch (direction) {
+    case 'up':
+      position.y++;
+      break;
+    case 'down':
+      position.y--;
+      break;
+    case 'left':
+      position.x--;
+      break;
+    case 'right':
+      position.x++;
+      break;
+    default:
+      throw Error('unknown command');
+  }
+  return position;
+}
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
